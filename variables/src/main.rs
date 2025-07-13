fbf9c108b0c0054b858efdcfe4e6eda50a365ef9
Gustavo da Silva Oliveira <silva.gustavo98@gmail.com>
2025-07-13 00:05:24 -0300

    Function de operacoes matematicas

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,10 +5,30 @@ fn shadowing() {
 
     {
         let x = x*2;
-        println!("O valor de x inner scope é: {x}")
+        println!("O valor de x inner scope é: {x}");
     }
 
-    println!("O valor de x fora de scope é: {x}")
+    println!("O valor de x fora de scope é: {x}");
+}
+
+fn math_operations() {
+    let sum = 5 + 10;
+    println!("Soma é {sum}");
+    
+    let sub = 10.0 - 5.5;
+    println!("Subtração é {sub}");
+
+    let product = 4 * 30;
+    println!("Produto é {product}");
+
+    let quotient = 56.7 / 32.2;
+    let truncated = -5/3;
+
+    println!("Quociente é {quotient}");
+    println!("Truncado é {truncated}");
+
+    let remainder = 10 % 5;
+    println!("Remainder é {remainder}");
 }
 
 fn main() {
@@ -19,4 +39,8 @@ fn main() {
 
     println!("Vamos rodar o shadowing");
     shadowing();
+    println!("----------------------------------");
+    println!("Função de Operações matemáticas");
+    math_operations();
+
 }
